519bf92e4af7a5cb9d3792c3908b3fbe3cb63f4a
benytocarlo <itoknot2004@gmail.com>
2014-07-21 21:14:21 -0400

    mod placeholder js

--- js/jquery.placeholder.js
+++ js/jquery.placeholder.js
@@ -1,17 +1,37 @@
-$(function(){    
-    if($.browser.msie && $.browser.version <= 9){
-        $("[placeholder]").focus(function(){
-            if($(this).val()==$(this).attr("placeholder")) $(this).val("");
-        }).blur(function(){
-            if($(this).val()=="") $(this).val($(this).attr("placeholder"));
-        }).blur();
+jQuery(function() {
+	   jQuery.support.placeholder = false;
+	   test = document.createElement('input');
+	   if('placeholder' in test) jQuery.support.placeholder = true;
+	});
+	// This adds placeholder support to browsers that wouldn't otherwise support it. 
+	$(function() {
+	   if(!$.support.placeholder) { 
+	      var active = document.activeElement;
+	      $(':text').focus(function () {
+	         if ($(this).attr('placeholder') != '' && $(this).val() == $(this).attr('placeholder')) {
+	            $(this).val('').removeClass('hasPlaceholder');
+	         }
+	      }).blur(function () {
+	         if ($(this).attr('placeholder') != '' && ($(this).val() == '' || $(this).val() == $(this).attr('placeholder'))) {
+	            $(this).val($(this).attr('placeholder')).addClass('hasPlaceholder');
+	         }
+	      });
+	      $(':text').blur();
+	      $(active).focus();
+	      $('form:eq(0)').submit(function () {
+	         $(':text.hasPlaceholder').val('');
+	      });
+	   }
+	});
 
-        $("[placeholder]").parents("form").submit(function() {
-            $(this).find('[placeholder]').each(function() {
-                if ($(this).val() == $(this).attr("placeholder")) {
-                    $(this).val("");
-                }
-            })
-        });
-    }
-});
+	$(document).ready(function(){
+	  	
+		$('[placeholder]').parents('form').submit(function() {
+		  $(this).find('[placeholder]').each(function() {
+		    var input = $(this);
+		    if (input.val() == input.attr('placeholder')) {
+		      input.val('');
+		    }
+		  })
+		});
+	});
